f1032ff9452368eabc654018cd9932e31894cc6d
Robert Brotherus <robert@iki.fi>
2024-10-02 20:06:24 +0300

    Adjust page layout and scaling

--- src/woolcat/styles.cljs
+++ src/woolcat/styles.cljs
@@ -1,8 +1,5 @@
 (ns woolcat.styles
-  (:require-macros [garden.def :refer [defcssfn defkeyframes]])
-  (:require [spade.core :refer [defglobal]]
-            [garden.units :as units]
-            [garden.stylesheet :refer [at-media]]))
+  (:require [spade.core :refer [defglobal]]))
 
 (def medium-font "20px")
 (def large-font "30px")
@@ -16,7 +13,7 @@
 (defglobal
   defaults
   [:body (assoc default-font :user-select "none" :padding "16px")]
-  [:div.main-title { :font-size "25px" }]
+  [:div.main-title {:font-size "25px"}]
   [:div.chinese-name {:font-family "Noto Sans HK", :font-size "20px",
                       :font-weight 500}]
   [:.logo-font {:font-family "fredericka the great" :font-size "90px"}]
@@ -30,16 +27,21 @@
   [:.margin {:margin "8px"}]
   [:.bold {:font-weight "bold"}]
   [:div.flex {:display "flex" :align-items "center"}]
+  ;; [:div.border {:border "solid 2px #4f4"}]
+  ;; [:div.border-blue {:border "solid 2px #88f"}]
   [:div.error {:display "grid" :grid-template-columns "1fr auto"
                :background "#f33" :color "black" :font-weight "bold"
                :padding "8px" :align-items "center"}]
+  [:div.main {:display "grid", :grid-template-columns "auto auto"
+              :max-width "1200px"
+              :margin-left "auto", :margin-right "auto"}]
   [:div.product-table
    {:display "grid"
-    :margin "auto"
     :margin-top "160px"
-    :grid-template-columns "1fr auto auto auto auto auto 1fr"
+    :grid-template-columns "auto auto auto auto auto"
     :gap "40px"}]
   [:div.crop-container {:width "200px" :height "200px" :overflow "hidden" :position "relative"}]
-  [:img.cropped-image {:width "100%" :height "100%" :object-fit "cover" }]
+  [:img.cropped-image {:width "100%" :height "100%" :object-fit "cover"}]
+  [:div.grid {:display "grid"}]
   )
 
--- src/woolcat/views.cljs
+++ src/woolcat/views.cljs
@@ -5,16 +5,18 @@
 
 (def img-base "https://rjb-share.s3.eu-north-1.amazonaws.com/woolcat-media/")
 
+(def wiggly-arrow [:span.large {:style {:position "relative" :top "2px"}} "⇝ "])
+
 (defn main-panel []
-  [:div
-   [:div.main-title "Chan Ann Chuang"]
-   [:div.chinese-name "莊誠安"]
+  [:div.main.border-blue
+   [:div.border
+    [:div.main-title "Chan Ann Chuang"]
+    [:div.chinese-name "莊誠安"]]
+   [:div.border {:style {:justify-self "end"}} "Info"]
    (into
-     [:div.product-table
-      [:div]]
+     [:div.product-table.border {:style {:grid-column "span 2"}}]
      (for [{:keys [name photo]} categories]
-       [:div
+       [:div.border-blue
         [:div.crop-container
          [:img.cropped-image {:src photo }]]
-        [:div.margin-top [:span.large {:style {:position "relative" :top "2px"}} "⇝ "] name]
-        ]))])
+        [:div.margin-top wiggly-arrow name]]))])
