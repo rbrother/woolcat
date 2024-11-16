46a9ee0dd30e98b861478b9b87db81118c869ed7
Robert Brotherus <robert@iki.fi>
2024-11-16 13:51:09 +0200

    Origami items added

--- src/woolcat/db.cljs
+++ src/woolcat/db.cljs
@@ -1,7 +1,6 @@
 (ns woolcat.db
-  (:require [re-frame.core :as rf]
-            [clojure.pprint :refer (pprint)]
-            [medley.core :refer [index-by]]))
+  (:require [medley.core :refer [index-by]]
+            [re-frame.core :as rf]))
 
 (def default-db
   {})
@@ -40,9 +39,9 @@
    {:name "Pink Chain-bag", :id "bag-pink-purple", :dimension "Fiber", :technique "Felt",
     :material #{"Wool" "Metal"}, :detail-pics 2}
    {:name "Qing", :id "qing-hat", :dimension "Yarn", :technique "Crochet",
-    :material "Wool", :detail-pics 2}
+    :material "Wool", :detail-pics 4}
    {:name "Song", :id "song-hat", :dimension "Yarn", :technique "Crochet",
-    :material #{"Cotton" "Metal"} :detail-pics 1}
+    :material #{"Cotton" "Metal"} :detail-pics 2}
    {:name "Rockstar", :id "rockstar-hat", :dimension "Yarn", :technique "Crochet",
     :material #{"Cotton" "Wool"}, :detail-pics 0}
    {:name "ZhuGe L", :id "zhuge-hat", :dimension "Yarn", :technique "Crochet",
@@ -59,8 +58,12 @@
     :material "Wool", :detail-pics 0}
    {:name "Rose", :id "rose", :dimension "Yarn", :technique "Crochet",
     :material #{"Cotton" "Wool" "Metal"}, :detail-pics 0}
+   {:name "Orchid", :id "orchid", :dimension "Yarn", :technique "Crochet",
+    :material #{"Cotton" "Wool" "Metal"}, :detail-pics 1}
+   {:name "Lily of Valley", :id "lily-of-valley", :dimension "Yarn", :technique "Crochet",
+    :material #{"Cotton" "Wool" "Metal"}, :detail-pics 1}
    {:name "Panda Bamboo Tapestry", :id "panda-bamboo-tapestry", :dimension #{"Yarn" "Fiber"}, :technique "Weave",
-    :material #{"Wool" "Cotton" "Wood"},  :detail-pics 0}
+    :material #{"Wool" "Cotton" "Wood"}, :detail-pics 0}
    {:name "Autumn Dress" :id "autumn-dress", :dimension "Textile", :technique "Sew", :detail-pics 2}
    {:name "Bear Coat" :id "bear-coat", :dimension "Textile", :technique "Sew", :detail-pics 3}
    {:name "Cheonsam" :id "cheonsam", :dimension "Textile", :technique "Sew", :detail-pics 2}
@@ -75,12 +78,18 @@
    {:name "Trench Coat" :id "trench-coat", :dimension "Textile", :technique "Sew", :detail-pics 3}
    {:name "Long Winter Coat" :id "winter-ao", :dimension "Textile", :technique "Sew", :detail-pics 4}
    {:name "Winter Coat" :id "winter-coat", :dimension "Textile", :technique "Sew", :detail-pics 1}
-
    {:name "Fox Hat 1" :id "fox-hat-1", :dimension "Skin", :technique "Sew", :detail-pics 7}
    {:name "Gray Mink Glove" :id "gray-mink-glove", :dimension "Skin", :technique "Sew", :detail-pics 4}
    {:name "White Mink Glove" :id "white-mink-glove", :dimension "Skin", :technique "Sew", :detail-pics 4}
    {:name "Fox Bag Charm" :id "fox-key-charm", :dimension "Skin", :technique "Sew", :detail-pics 3}
-
+   {:name "Hare Bag" :id "hare-bag", :dimension "Skin", :technique "Sew", :detail-pics 5}
+   {:name "Lamp Cover 1" :id "lamp-cover-1", :dimension "Paper", :technique "Origami", :detail-pics 1}
+   {:name "Lamp Cover 2" :id "lamp-cover-2", :dimension "Paper", :technique "Origami", :detail-pics 1}
+   {:name "Organic Lamp Cover" :id "organic-lamp-cover", :dimension "Paper", :technique "Origami", :detail-pics 1}
+   {:name "Asian Lamp Cover" :id "asian-lamp-cover", :dimension "Paper", :technique "Origami", :detail-pics 1}
+   {:name "Bunny and Chicken" :id "bunny-and-chicken", :dimension "Paper", :technique "Origami", :detail-pics 2}
+   {:name "Cactus" :id "cactus", :dimension "Paper", :technique "Origami", :detail-pics 2}
+   {:name "Ruby" :id "ruby", :dimension "Paper", :technique "Origami", :detail-pics 2}
    ])
 
 (def products (mapv amend-product products-raw))
