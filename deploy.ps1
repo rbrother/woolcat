319b663e976bcb225a1522a450b659daa6f94eb1
Robert Brotherus <robert@iki.fi>
2025-01-06 19:32:53 +0200

    deploy with no-cache headers to prevent seeing old versions

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,5 @@
 npm install
 Remove-Item -Recurse -Force .\resources\public\js\compiled
 npm run release
-aws s3 cp .\resources\public  s3://woolcat/ --recursive
+aws s3 cp .\resources\public  s3://woolcat/ --recursive --cache-control "no-cache, no-store, must-revalidate"
 # Deploys to: http://woolcat.s3-website.eu-north-1.amazonaws.com/
