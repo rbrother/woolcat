7bf6f61b4b2b210f4d875f126e46d0580344146d
Robert Brotherus <robert@iki.fi>
2025-01-11 21:19:13 +0200

    Change deployment to woolcat.fi bucket in preparation to AWS CloudFront deployment

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,6 @@
 npm install
 Remove-Item -Recurse -Force .\resources\public\js\compiled
 npm run release
-aws s3 cp .\resources\public  s3://woolcat/ --recursive --cache-control "no-cache, no-store, must-revalidate"
+aws s3 cp .\resources\public  s3://woolcat.fi/ --recursive --cache-control "no-cache, no-store, must-revalidate"
 # Deploys to: http://woolcat.fi.s3-website.eu-north-1.amazonaws.com/
 # Mapped to Cloudfront disribution: https://d2l603lgj6s6a9.cloudfront.net/
